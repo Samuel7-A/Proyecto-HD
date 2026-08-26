852dcc1f28e092ff6b904ffc5c119a9b503d985d
LAB-USR-LSUR <LAB-USR-LSUR@grupoutp.net>
2026-08-26 16:47:45 -0500

    Creación del proyecto de Herramientas de Desarrollo

---
+++ lancenterhd/src/main/java/com/lancenter/lancenterhd/LancenterhdApplication.java
@@ -0,0 +1,13 @@
+package com.lancenter.lancenterhd;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LancenterhdApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LancenterhdApplication.class, args);
+	}
+
+}
---
+++ lancenterhd/src/test/java/com/lancenter/lancenterhd/LancenterhdApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lancenter.lancenterhd;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LancenterhdApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
